fb7fcf1d4da9034a829dcbaa5b804ab94ef19bc8
Kevin Puscán Ortiz <kevinjpuscan@gmail.com>
2023-04-30 17:23:10 +0200

    ci(prettier): add prettier

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: "Notes Manager",
+  description: "Your notes manager",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
+  return (
+    <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+import "@/styles/globals.css";
+
+export default function App(): JSX.Element {
+  return <div>Hola mundo</div>;
+}
